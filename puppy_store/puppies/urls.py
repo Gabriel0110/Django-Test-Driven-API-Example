71f93715bd36c4dc5a595237fd75650e1a34a443
Gabriel Tomberlin <gabrieltomberlin14@gmail.com>
2021-04-13 19:55:49 -0500

    Update urls.py in puppies app folder to convert 'url' to 'path' for Django 2.0

--- puppy_store/puppies/urls.py
+++ puppy_store/puppies/urls.py
@@ -1,14 +1,14 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(
-        r'^api/v1/puppies/(?P<pk>[0-9]+)$',
+    path(
+        'api/v1/puppies/<int:pk>',
         views.get_delete_update_puppy,
         name='get_delete_update_puppy'
     ),
-    url(
-        r'^api/v1/puppies/$',
+    path(
+        'api/v1/puppies/',
         views.get_post_puppies,
         name='get_post_puppies'
     )
