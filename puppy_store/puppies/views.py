77f412162d473b8c064bceb0ab934aacae40fd7d
Gabriel Tomberlin <gabrieltomberlin14@gmail.com>
2021-04-13 19:43:20 -0500

    Create a serializer for the Puppy model, initalize APIClient app for view testing in test_views.py, and create skeleton of all view functions that return empty responses to then map to appropriate URLs next.

--- puppy_store/puppies/views.py
+++ puppy_store/puppies/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Puppy
+from .serializers import PuppySerializer
 
 # Create your views here.
+
+@api_view(['GET', 'DELETE', 'PUT'])
+def get_delete_update_puppy(request, pk):
+    try:
+        puppy = Puppy.objects.get(pk=pk)
+    except Puppy.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    
+    if request.method == 'GET': # get details of a single puppy
+        return Response({})
+    elif request.method == 'DELETE': # delete a single puppy
+        return Response({})
+    elif request.method == 'PUT': # update details of a single puppy
+        return Response({})
+
+@api_view(['GET', 'POST'])
+def get_post_puppies(request):
+    if request.method == 'GET': # get all puppies
+        return Response({})
+    elif request.method == 'POST': # insert a new record for a puppy
+        return Response({})
